183ce5aad39f6d63a4c7cf793ddd97564c63598b
walter <desing.fotografia.m@gmail.com>
2026-09-01 19:09:13 -0500

    feat: agregar soporte PWA para instalación en celulares

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [],
-  },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
