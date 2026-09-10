475a6019060a4cf9c89ac56030abb95597879eb0
walter <desing.fotografia.m@gmail.com>
2026-09-10 09:40:47 -0500

    fix: desactivar PWA para corregir carga movil

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
